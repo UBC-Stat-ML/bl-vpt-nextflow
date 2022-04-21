9fae1bd5e24709e4d5e85cae1d91228633208f7a
Alexandre Bouchard-Cote <alexbou@login01.sockeye.arc.ubc.ca>
2022-04-21 07:48:55 -0700

    Auto: add deliverables

--- deliverables/pt_topologies/rerun.sh
+++ deliverables/pt_topologies/rerun.sh
@@ -4,11 +4,10 @@ require("dplyr")
 require("ggridges")
 
 ks_distances <- read.csv("ks_distances.csv")
-ks_distances$quality <- ifelse(ks_distances$ks_stat > 0.2, "poor", "good")
+ks_distances$quality <- ifelse(ks_distances$ks_stat > 0.1, "poor", "good")
 
 restarts <- read.csv("aggregated/actualTemperedRestarts.csv.gz")
 restarts <- restarts %>% inner_join(ks_distances, by = c("algorithm", "model", "seed"))
-
 restarts %>%
   filter(algorithm != "Reference") %>%
   group_by(quality, model, algorithm) %>%
@@ -22,6 +21,26 @@ restarts %>%
     theme_bw() +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 ggsave("actualTemperedRestarts-box.pdf", width = 5, height = 8)
+
+ess <- read.csv("aggregated/allEss.csv.gz")
+ess <- ess %>% inner_join(ks_distances, by = c("algorithm", "model", "seed"))
+ess %>%
+  filter(algorithm != "Reference") %>%
+  filter(variable != "allLogDensities") %>%
+  filter(variable != "energy") %>%
+  filter(variable != "nOutOfSupport") %>%
+  filter(variable != "otherAnnealed") %>%
+  group_by(quality, model, algorithm, variable) %>%
+  summarize(total_ess = sum(value) ) %>%
+  ggplot(aes(x = algorithm, y = total_ess, color = quality)) +
+    facet_grid(model ~ ., scales="free_y") +
+    geom_boxplot() +
+    ylab("ESS") + 
+    scale_y_continuous(expand = expansion(mult = 0.05), limits = c(0, NA)) +
+    scale_fill_manual(values = c(  "good" = "blue", "poor" = "red")) + scale_colour_manual(values = c(  "good" = "blue", "poor" = "red")) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave("ess-box.pdf", width = 5, height = 8)
 
 global <- read.csv("aggregated/globalLambda.csv.gz")
 global <- global %>% inner_join(ks_distances, by = c("algorithm", "model", "seed"))
